88cd6089d6fef5155e8b9425320df3b8470a592f
dingyif <54865162+dingyif@users.noreply.github.com>
2020-01-23 14:45:55 -0500

    Update Comment

--- detect_by_contour_v3.py
+++ detect_by_contour_v3.py
@@ -46,7 +46,8 @@ img_re_idx = 0 #relative index for images in start_img_idx to end_img_idx
 for filename in img_paths[start_img_idx-1:end_img_idx]: #original img: 958 rowsx646 cols
     img=Image.open(filename).convert('L') #.convert('L'): gray-scale # 646x958
     img_array=np.float32(img) #convert from Image object to numpy array (black 0-255 white); 958x646
-    if img_re_idx==0:
+    #pu in the correct data structure
+    if img_re_idx == 0:
         img_nrow = img_array.shape[0]
         img_ncol = img_array.shape[1]
         img_stack = np.ndarray((img_num,img_nrow,img_ncol), dtype=np.float32)
@@ -66,6 +67,7 @@ if is_stem==False:
     diff_stack= -diff_stack#!!! don't know why, but this is needed the leafs from the diff_sum graphs
 diff_pos_stack = (diff_stack >= 0)*diff_stack #discard the negative ones
 
+#read the tiff file
 true_diff  = tiff.imread(img_folder+'/2 Image Difference Substack ('+str(start_img_idx)+'-'+str(end_img_idx-1)+').tif')
 if is_save==True:
     diff_combined = np.concatenate((true_diff,diff_pos_stack.astype(np.uint8)),axis=2)
@@ -132,19 +134,20 @@ if is_stem==True:
         plt.imsave(img_folder + "/m1_mean_of_binary_img.jpg",mean_img,cmap='gray')
 
 def extract_foreground(img_2d,blur_radius=10.0,fg_threshold=0.1,expand_radius_ratio=5,is_save=False):
-    #smooth img_2d by gaussian filter w/blur radius
-    #background when smoothed img > fg_threhold
-    #expand the foreground object area by applying uniform filter w/radius = 
-    #        blur_radiu*expand_radius_ratio then binarize it
-    #expansion is made to be more conservative
-    #displays two image, 1st img is how much expansion of foreground is done
-    #       2nd img is the foreground(True) background(False) result
-    #return a 2d logical array of the same size as img_2d
+    '''
+    smooth img_2d by gaussian filter w/blur radius
+    background when smoothed img > fg_threhold
+    expand the foreground object area by applying uniform filter 
+    radius = blur_radiu*expand_radius_ratio then binarize it
+    expansion is made to be more conservative
+    displays two image, 1st img is how much expansion of foreground is done
+           2nd img is the foreground(True) background(False) result
+    return a 2d logical array of the same size as img_2d
     
-    #fg_threshold assumes embolism only happens at 
-    #the same given pixel at most 10% of the time
-    #i.e. this is an assumption on how much xylem channels overlay
-    
+    fg_threshold assumes embolism only happens at 
+    the same given pixel at most 10% of the time
+    i.e. this is an assumption on how much xylem channels overlay
+    '''
     
     #smoothing (gaussian filter)
     smooth_img = ndimage.gaussian_filter(img_2d, blur_radius)
@@ -166,18 +169,19 @@ def extract_foreground(img_2d,blur_radius=10.0,fg_threshold=0.1,expand_radius_ra
 
 if is_stem==True:
     is_stem_mat = extract_foreground(mean_img,expand_radius_ratio=8,is_save=True)
-    
-    # the above is_stem_mat might be too big
-    # (one matrix that determines whether it's stem for all images)
-    # for each img, 
-    #    use the fact that stem is brighter than background from original img 
-    #    --> threshold by mean of each image to get another estimate of whether 
-    #        it's stem or not for each img
-    #    --> then intersect with is_stem_mat, to get final the final is_stem_mat2 
+    '''
+    the above is_stem_mat might be too big
+    (one matrix that determines whether it's stem for all images)
+    for each img, 
+        use the fact that stem is brighter than background from original img 
+        --> threshold by mean of each image to get another estimate of whether 
+        it's stem or not for each img
+        --> then intersect with is_stem_mat, to get final the final is_stem_mat2 
+    '''
     mean_each_img = np.mean(np.mean(img_stack,axis=2),axis=1)#mean of each image
     mean_each_stack = np.repeat(mean_each_img[:,np.newaxis],img_stack.shape[1],1)
     mean_each_stack = np.repeat(mean_each_stack[:,:,np.newaxis],img_stack.shape[2],2) 
-    bigger_than_mean =  img_stack>mean_each_stack#thresholded by mean
+    bigger_than_mean =  img_stack > mean_each_stack#thresholded by mean
     is_stem_mat2 = bigger_than_mean[:-1,:,:]*(is_stem_mat*1)
     #drop the last img s.t. size would be the same as diff_stack
     #multiply by is_stem_mat to crudely remove the noises (false positive) outside of is_stem_mat2
@@ -206,21 +210,19 @@ def find_emoblism_by_contour(bin_stack,img_idx,stem_area,final_area_th = 20000/2
     if plot_interm == True:
         plot_gray_img(closing_e,str(img_idx)+"_closing_e")
     
-    contours, hierarchy = cv2.findContours(closing_e,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
+    contours, _ = cv2.findContours(closing_e,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
     #find contours with simple approximation
     
-    areas = [] #list to hold all areas
-    
-    for contour in contours:
-      ar = cv2.contourArea(contour)
-      areas.append(ar)
+    #list to hold all areas
+    areas = [cv2.contourArea(contour) for contour in contours]
     
     #remove particles with area < area_th
-    area_index = [i for i,val in enumerate(areas) if val>=area_th]#extract the index in areas with areas>=area_th
+    area_index = [i for i,val in enumerate(areas) if val >= area_th]#extract the index in areas with areas >= area_th
     #enumerate is used in case there are multiply indices with the same areas value
     
     if len(area_index) == 0:#no particles with area >= area_th
         return(bin_stack[img_idx,:,:]*0)#just return no embolism
+    
     else:
         cnt = operator.itemgetter(*area_index)(contours)
         
@@ -245,21 +247,21 @@ def find_emoblism_by_contour(bin_stack,img_idx,stem_area,final_area_th = 20000/2
         
         ################### step 2: shrink the embolsim parts (more false negative) ###################
         #    erosion(e2_sz*e2_sz) and opening(o2_sz*o2_sz) and then closing(cl2_sz*cl2_sz) [closing2]
-        #    --> keep contours with area>area_th2 and fill in the contour by polygons [contour_mask2]
+        #    --> keep contours with area > area_th2 and fill in the contour by polygons [contour_mask2]
         ################################################################################################
         kernel_e2 = np.ones((e2_sz,e2_sz),np.uint8) #square image kernel used for erosion
         erosion2 = cv2.erode(bin_stack[img_idx,:,:].astype(np.uint8), kernel_e2,iterations = 1) #refines all edges in the binary image
         if plot_interm == True:
-            plot_gray_img(erosion2,str(img_idx)+"_erosion2")
+            plot_gray_img(erosion2, str(img_idx) + "_erosion2")
         
         kernel_o2 = np.ones((o2_sz,o2_sz),np.uint8)
         #https://homepages.inf.ed.ac.uk/rbf/HIPR2/open.htm
         opening = cv2.morphologyEx(erosion2.astype(np.uint8), cv2.MORPH_OPEN, kernel_o2)
         #tends to remove some of the foreground (bright) pixels from the edges of regions of foreground pixels.
-        # However it is less destructive than erosion in general.
+        #However it is less destructive than erosion in general.
         
         if plot_interm == True:
-            plot_gray_img(opening,str(img_idx)+"_opening2")
+            plot_gray_img(opening, str(img_idx) + "_opening2")
         
         #the closing here is needed for cases like ALCLAT1_leaf Subset: img_idx = 57
         #where embolism is break into small chunks and disappear mostly after opening
@@ -267,17 +269,15 @@ def find_emoblism_by_contour(bin_stack,img_idx,stem_area,final_area_th = 20000/2
         closing2 = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel_cl2)
         if plot_interm == True:
             plot_gray_img(closing2,str(img_idx)+"_closing2")
+
+        #find contours with simple approximation
+        contours2, hierarchy2 = cv2.findContours(closing2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
         
-        contours2, hierarchy2 = cv2.findContours(closing2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #find contours with simple approximation
-        
-        areas2 = [] #list to hold all areas
-        
-        for contour2 in contours2:
-          ar = cv2.contourArea(contour2)
-          areas2.append(ar)
+        #list to hold all areas
+        areas2 = [cv2.contourArea(contour2) for contour2 in contours2]
          
         #remove particles with area < area_th2
-        area_index2 = [i for i,val in enumerate(areas2) if val>=area_th2]#extract the index in areas with areas>=area_th
+        area_index2 = [i for i,val in enumerate(areas2) if val >= area_th2]#extract the index in areas with areas>=area_th
         #enumerate is used in case there are multiply indices with the same areas value
         
         if len(area_index2) == 0:#no particles with area >= area_th
@@ -312,12 +312,14 @@ def find_emoblism_by_contour(bin_stack,img_idx,stem_area,final_area_th = 20000/2
                         print("number of pixel added/area of that connected component in dilate_img",np.sum(add_part)/np.sum(mat_cc == cc_label))
                     #print(str(img_idx)+":"+str(np.sum(add_part)/area_cm2))
                     if np.sum(add_part)/area_cm2 < ratio_th:# and np.sum(add_part)/np.sum(mat_cc == cc_label)>0.19:
-                        #1st condition
-                        #avoid the case that a small noise in contour_mask leads to a really big "add_part"
-                        #that are essentially just noises
-                        #case: ALCLAT1_leaf Subset: img_idx = 190
-                        #2nd condition
-                        #crude way of discarding parts with low density
+                        '''
+                        1st condition
+                        avoid the case that a small noise in contour_mask leads to a really big "add_part"
+                        that are essentially just noises
+                        case: ALCLAT1_leaf Subset: img_idx = 190
+                        2nd condition
+                        crude way of discarding parts with low density
+                        '''
                         final_mask = final_mask + add_part
                     
             final_img = np.copy(final_mask)
@@ -330,14 +332,16 @@ def find_emoblism_by_contour(bin_stack,img_idx,stem_area,final_area_th = 20000/2
             #        OR the embolism area is too small
             #    --> treat as if there's no embolsim in the entire image
             ################################################################################################
-            if np.sum(final_img)/stem_area>shift_th or np.sum(final_img)<final_area_th:
-                #percentage of embolism in the stem_area is too large
-                #probably is false positive due to shifting of img (case: stem img_idx=66,67)
-                #remove false positive with really small sizes
-                final_img = final_img*0
+            if np.sum(final_img)/stem_area > shift_th or np.sum(final_img) < final_area_th:
+                '''
+                percentage of embolism in the stem_area is too large
+                probably is false positive due to shifting of img (case: stem img_idx=66,67)
+                remove false positive with really small sizes
+                '''
+                final_img = final_img * 0
             
             
-#            #reduce false positive, discard those with small density, TODO: dilate/closing first?
+            #reduce false positive, discard those with small density, TODO: dilate/closing first?
             final_img_cp = np.copy(final_img)#w/o np.copy, changes made in final_img_cp will affect final_img
             closing_3 = cv2.morphologyEx(final_img_cp.astype(np.uint8), cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
             if plot_interm == True:
@@ -394,18 +398,23 @@ bin_stem_stack = bin_stack*is_stem_mat2
 
 for img_idx in range(0,bin_stack.shape[0]):
     stem_area = np.sum(is_stem_mat2[img_idx,:,:])
-    final_stack1[img_idx,:,:] = find_emoblism_by_contour(bin_stem_stack,img_idx,stem_area=stem_area,final_area_th = final_area_th,area_th=area_th, area_th2=area_th2,ratio_th=35,e2_sz=1,o2_sz=2,cl2_sz=2,plot_interm=False,shift_th=shift_th,density_th=density_th,num_px_th=num_px_th)
+    final_stack1[img_idx,:,:] = find_emoblism_by_contour(bin_stem_stack,img_idx,stem_area = stem_area,final_area_th = final_area_th,
+                                                        area_th=area_th, area_th2=area_th2,ratio_th=35,e2_sz=1,o2_sz=2,cl2_sz=2,
+                                                        plot_interm=False,shift_th=shift_th,density_th=density_th,num_px_th=num_px_th)
+        
+        '''
         #find_emoblism_by_contour(bin_stack,57,area_th=30, area_th2=30,ratio_th=100,e2_sz=1,o2_sz=3,cl2_sz=2,plot_interm=True)#for leaf ALC
-#    #debugging:
-#    bin_stack = bin_stem_stack
-#    ratio_th=35
-#    e2_sz=1
-#    o2_sz=2
-#    cl2_sz=2
-#    plot_interm=False
+    debugging:
+    bin_stack = bin_stem_stack
+    ratio_th=35
+    e2_sz=1
+    o2_sz=2
+    cl2_sz=2
+    plot_interm=False
     
-#    if np.any(final_stack[img_idx,:,:]):
-#        has_embolism[img_idx] = 1
+    if np.any(final_stack[img_idx,:,:]):
+        has_embolism[img_idx] = 1
+'''
 
 #############################################################################
 #Don't count as embolism if it keeps appearing (probably is plastic cover)
@@ -519,45 +528,46 @@ def confusion_mat_img(has_embolism,true_has_emb):
     return([con_df,fp_idx,fn_idx,tp_idx,tn_idx])
 
 con_img_list = confusion_mat_img(has_embolism,true_has_emb)
-print(con_img_list[0])#confusion matrix at img-level
+print(con_img_list[0])
+#confusion matrix at img-level
+'''
+Tuned/Train using leaf ALC, testing by leaf CAS
 
-#Tuned/Train using leaf ALC, testing by leaf CAS
+Leaf ALC
+        Predict 0  Predict 1
+True 0      137.0       24.0
+True 1        2.0       28.0
 
-#Leaf ALC
-#        Predict 0  Predict 1
-#True 0      137.0       24.0
-#True 1        2.0       28.0
+Leaf CAS
+        Predict 0  Predict 1
+True 0      302.0        4.0
+True 1       15.0        3.0
 
-#Leaf CAS
-#        Predict 0  Predict 1
-#True 0      302.0        4.0
-#True 1       15.0        3.0
+Stem
+        Predict 0  Predict 1
+True 0      295.0       17.0
+True 1        2.0       35.0
 
-#Stem
-#             Predict 0  Predict 1
-#True 0      295.0       17.0
-#True 1        2.0       35.0
+v3:1+4+5
+        Predict 0  Predict 1
+True 0      253.0       59.0
+True 1        0.0       37.0
 
-#v3:1+4+5
-#        Predict 0  Predict 1
-#True 0      253.0       59.0
-#True 1        0.0       37.0
+v3:1+4+5+6
+        Predict 0  Predict 1
+True 0      266.0       46.0
+True 1        0.0       37.0
 
-#v3:1+4+5+6
-#        Predict 0  Predict 1
-#True 0      266.0       46.0
-#True 1        0.0       37.0
+v3:1+4+5+6+7 (final_area_th2 = 80)
+        Predict 0  Predict 1
+True 0      294.0       18.0
+True 1        0.0       37.0
 
-#v3:1+4+5+6+7 (final_area_th2 = 80)
-#        Predict 0  Predict 1
-#True 0      294.0       18.0
-#True 1        0.0       37.0
-
-#v3:1+4+5+6+7 (final_area_th2 = 120)
-#        Predict 0  Predict 1
-#True 0      303.0        9.0
-#True 1        3.0       34.0
-
+v3:1+4+5+6+7 (final_area_th2 = 120)
+        Predict 0  Predict 1
+True 0      303.0        9.0
+True 1        3.0       34.0
+'''
 print("false positive img index",con_img_list[1])
 print("false negative img index",con_img_list[2])
 
@@ -612,38 +622,40 @@ def confusion_mat_pixel(pred_stack,true_stack):
 con_df_px = confusion_mat_pixel(final_stack,true_mask)
 print(con_df_px)
 
+'''
 #######ALCLAT1_leaf Subset
-#          Predict 0  Predict 1
-#True 0  235408544.0    75285.0
-#True 1      16434.0    55066.0
+          Predict 0  Predict 1
+True 0  235408544.0    75285.0
+True 1      16434.0    55066.0
 
 ####### Leaf CAS
-#         Predict 0  Predict 1
-#True 0  399552512.0     7427.0
-#True 1      14448.0     6373.0
+          Predict 0  Predict 1
+True 0  399552512.0     7427.0
+True 1      14448.0     6373.0
 
 #######Stem
-#final_area_th
-#          Predict 0  Predict 1
-#True 0  215905376.0    14504.0
-#True 1      14121.0    50923.0
+final_area_th
+          Predict 0  Predict 1
+True 0  215905376.0    14504.0
+True 1      14121.0    50923.0
 
-#v3:1+4+5
-#          Predict 0  Predict 1
-#True 0  215894944.0    24942.0
-#True 1      11707.0    53337.0
+v3:1+4+5
+          Predict 0  Predict 1
+True 0  215894944.0    24942.0
+True 1      11707.0    53337.0
 
-#v3:1+4+5+6
-#          Predict 0  Predict 1
-#True 0  215899040.0    20840.0
-#True 1      12251.0    52793.0
+v3:1+4+5+6
+          Predict 0  Predict 1
+True 0  215899040.0    20840.0
+True 1      12251.0    52793.0
 
-#v3:1+4+5+6+7 (final_area_th2 = 80)
-#          Predict 0  Predict 1
-#True 0  215900320.0    19574.0
-#True 1      12251.0    52793.0
+v3:1+4+5+6+7 (final_area_th2 = 80)
+          Predict 0  Predict 1
+True 0  215900320.0    19574.0
+True 1      12251.0    52793.0
 
-#v3:1+4+5+6+7 (final_area_th2 = 120)
-#          Predict 0  Predict 1
-#True 0  215901312.0    18575.0
-#True 1      12431.0    52613.0
+v3:1+4+5+6+7 (final_area_th2 = 120)
+        Predict 0  Predict 1
+True 0  215901312.0    18575.0
+True 1      12431.0    52613.0
+'''
